5081d08b7499e593e532c62871276fa4e583ad95
solareenlo <solareenlo@gmail.com>
2021-04-16 21:11:32 +0900

    add: ft_run_op_dq

---
+++ src_push_swap/ft_run_op_dq.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_run_op_dq.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tayamamo <tayamamo@student.42tokyo.>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/15 19:06:05 by tayamamo          #+#    #+#             */
+/*   Updated: 2021/04/16 20:07:34 by tayamamo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+static	void	_run_rrr(t_deque *a, t_deque *b, int op)
+{
+	int	tmp;
+
+	if (op == RRA || op == RRR)
+	{
+		tmp = ft_deque_get_back(a);
+		ft_deque_pop_back(a);
+		ft_deque_push_front(a, tmp);
+	}
+	if (op == RRB || op == RRR)
+	{
+		tmp = ft_deque_get_back(b);
+		ft_deque_pop_back(b);
+		ft_deque_push_front(b, tmp);
+	}
+}
+
+static	void	_run_rr(t_deque *a, t_deque *b, int op)
+{
+	int	tmp;
+
+	if (op == RA || op == RR)
+	{
+		tmp = ft_deque_get_front(a);
+		ft_deque_pop_front(a);
+		ft_deque_push_back(a, tmp);
+	}
+	if (op == RB || op == RR)
+	{
+		tmp = ft_deque_get_front(b);
+		ft_deque_pop_front(b);
+		ft_deque_push_back(b, tmp);
+	}
+}
+
+void	ft_run_op_dq(t_deque *a, t_deque *b, int op)
+{
+	int	tmp;
+
+	if (op == SA || op == SS)
+		ft_deque_swap_front(a);
+	if (op == SB || op == SS)
+		ft_deque_swap_front(b);
+	if (op == PA)
+	{
+		tmp = ft_deque_get_front(b);
+		ft_deque_pop_front(b);
+		ft_deque_push_front(a, tmp);
+	}
+	if (op == PB)
+	{
+		tmp = ft_deque_get_front(a);
+		ft_deque_pop_front(a);
+		ft_deque_push_front(b, tmp);
+	}
+	_run_rr(a, b, op);
+	_run_rrr(a, b, op);
+}
