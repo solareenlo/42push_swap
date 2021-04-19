d7cde1faa1dd5b3c75c7291546d271b1f4442bf2
solareenlo <solareenlo@gmail.com>
2021-04-20 03:34:09 +0900

    fix: ft_dq_dfs_a

--- src_push_swap/ft_dq_dfs_a.c
+++ src_push_swap/ft_dq_dfs_a.c
@@ -6,13 +6,13 @@
 /*   By: tayamamo <tayamamo@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/19 05:00:20 by tayamamo          #+#    #+#             */
-/*   Updated: 2021/04/19 17:04:50 by tayamamo         ###   ########.fr       */
+/*   Updated: 2021/04/20 03:16:40 by tayamamo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static int	_run_op(t_dq *dq, t_deque *op)
+static int	_run_op(t_dq *dq, int n, t_deque *op)
 {
 	t_dq	*tmp_dq;
 	t_deque	*tmp_op;
@@ -30,10 +30,7 @@ static int	_run_op(t_dq *dq, t_deque *op)
 		ft_deque_pop_front(tmp_op);
 		ft_op_run_dq(tmp_dq, ope);
 	}
-	ret = ft_deque_is_sorted(tmp_dq->a, tmp_dq->cmp_a);
-	if (ft_deque_get_size(tmp_dq->a, tmp_dq->a->front, tmp_dq->a->back)
-		!= tmp_dq->a->size)
-		ret = 0;
+	ret = ft_deque_is_sorted_front(tmp_dq->a, n, tmp_dq->cmp_a);
 	ft_deque_free(tmp_op);
 	ft_dq_free(tmp_dq);
 	return (ret);
@@ -53,12 +50,11 @@ void	ft_dq_dfs_a(t_dq *dq, int n, t_deque *op[])
 
 	size = ft_deque_get_size(op[0], op[0]->front, op[0]->back);
 	mini = ft_deque_get_size(op[1], op[1]->front, op[1]->back);
-	if (size >= dq->a->size * 2 - 1)
+	if (size >= n * 2 - 1)
 		return ;
-	if (ft_deque_is_sorted(dq->a, dq->cmp_a))
-		if (ft_deque_get_size(dq->a, dq->a->front, dq->a->back) == dq->a->size)
-			return ;
-	if (_run_op(dq, op[0]) == 1)
+	if (ft_deque_is_sorted_front(dq->a, n, dq->cmp_a))
+		return ;
+	if (_run_op(dq, n, op[0]) == 1)
 	{
 		if (mini > ft_deque_get_size(op[0], op[0]->front, op[0]->back))
 			ft_deque_copy(op[1], op[0]);
