8bdec6cc3772e07b475f8a5ae8b403c0dc5325c4
solareenlo <solareenlo@gmail.com>
2021-04-25 00:24:30 +0900

    fix: ft_dq_sort

--- src_push_swap/ft_dq_sort.c
+++ src_push_swap/ft_dq_sort.c
@@ -6,7 +6,7 @@
 /*   By: tayamamo <tayamamo@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/16 16:34:22 by tayamamo          #+#    #+#             */
-/*   Updated: 2021/04/24 05:30:22 by tayamamo         ###   ########.fr       */
+/*   Updated: 2021/04/24 23:11:19 by tayamamo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,18 +64,7 @@ void	ft_dq_sort(t_dq *dq)
 		ft_deque_pop_front(n);
 		ret = ft_dq_cmp_with_med_from_a_to_b(dq, size);
 		ft_deque_push_front(n, ret);
-	ft_putendl_fd("A2", 1);
-	ft_deque_put(dq->a);
-	ft_putendl_fd("B2", 1);
-	ft_deque_put(dq->b);
 		ft_dq_sort_pa_ra(dq);
-	ft_putendl_fd("A3", 1);
-	ft_deque_put(dq->a);
-	ft_putendl_fd("B3", 1);
-	ft_deque_put(dq->b);
-	ft_putstr_fd("n:", 1);
-	ft_deque_put(n);
-	ft_deque_put(dq->sorted);
 		ft_dq_sort_ra(dq, n);
 		ft_deque_free(n);
 	}
