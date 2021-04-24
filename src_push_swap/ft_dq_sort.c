898d81c422176ac5943762d41a1e6198cc8e2a74
solareenlo <solareenlo@gmail.com>
2021-04-25 04:42:58 +0900

    update: ft_dq_sort_less_then_5

--- src_push_swap/ft_dq_sort.c
+++ src_push_swap/ft_dq_sort.c
@@ -6,7 +6,7 @@
 /*   By: tayamamo <tayamamo@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/16 16:34:22 by tayamamo          #+#    #+#             */
-/*   Updated: 2021/04/24 23:11:19 by tayamamo         ###   ########.fr       */
+/*   Updated: 2021/04/25 04:30:06 by tayamamo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ void	ft_dq_sort(t_dq *dq)
 		ft_exit("Error\n");
 	ft_dq_sort_init_op(op[1]);
 	if (dq->a->size <= 5)
-		ft_dq_sort_less_than_5(dq, op);
+		ft_dq_sort_less_than_5(dq);
 	else if (dq->a->size <= 10)
 		ft_dq_sort_less_than_10(dq, op);
 	else
--- src_push_swap/ft_dq_sort_less_than_5.c
+++ src_push_swap/ft_dq_sort_less_than_5.c
@@ -6,18 +6,28 @@
 /*   By: tayamamo <tayamamo@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/21 19:04:03 by tayamamo          #+#    #+#             */
-/*   Updated: 2021/04/21 19:05:06 by tayamamo         ###   ########.fr       */
+/*   Updated: 2021/04/25 04:29:30 by tayamamo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	ft_dq_sort_less_than_5(t_dq *dq, t_deque *op[])
+void	ft_dq_sort_less_than_5(t_dq *dq)
 {
-	int	size;
+	t_deque	*op[2];
+	int		size;
 
+	op[0] = ft_deque_init(OPSIZE);
+	if (op[0] == NULL)
+		ft_exit("Error\n");
+	op[1] = ft_deque_init(OPSIZE);
+	if (op[1] == NULL)
+		ft_exit("Error\n");
+	ft_dq_sort_init_op(op[1]);
 	size = ft_deque_get_size(dq->a, dq->a->front, dq->a->back);
 	ft_dq_dfs_a(dq, size, op);
 	ft_op_put_dq(op[1]);
 	ft_op_run_dq2(dq, op[1]);
+	ft_deque_free(op[0]);
+	ft_deque_free(op[1]);
 }
