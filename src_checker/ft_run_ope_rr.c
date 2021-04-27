c642b3f30271a05473f6ae61f4b893de00657579
solareenlo <solareenlo@gmail.com>
2021-04-28 05:19:24 +0900

    update: ft_run_op*

--- src_checker/ft_run_ope_rr.c
+++ src_checker/ft_run_ope_rr.c
@@ -6,50 +6,63 @@
 /*   By: tayamamo <tayamamo@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/28 05:04:02 by tayamamo          #+#    #+#             */
-/*   Updated: 2021/04/28 05:04:46 by tayamamo         ###   ########.fr       */
+/*   Updated: 2021/04/28 05:19:10 by tayamamo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "checker.h"
 
-void	ft_run_ope_rr(t_deque *a, t_deque *b, char *op)
+static void	_run_ra(t_deque *a, t_deque *b)
 {
 	int	tmp;
 
+	if (ft_deque_is_empty(a) == 0)
+	{
+		tmp = ft_deque_get_front(a);
+		ft_deque_pop_front(a);
+		ft_deque_push_back(a, tmp);
+	}
+	ft_display_stack(a, b);
+}
+
+static void	_run_rb(t_deque *a, t_deque *b)
+{
+	int	tmp;
+
+	if (ft_deque_is_empty(b) == 0)
+	{
+		tmp = ft_deque_get_front(b);
+		ft_deque_pop_front(b);
+		ft_deque_push_back(b, tmp);
+	}
+	ft_display_stack(a, b);
+}
+
+static void	_run_rr(t_deque *a, t_deque *b)
+{
+	int	tmp;
+
+	if (ft_deque_is_empty(a) == 0)
+	{
+		tmp = ft_deque_get_front(a);
+		ft_deque_pop_front(a);
+		ft_deque_push_back(a, tmp);
+	}
+	if (ft_deque_is_empty(b) == 0)
+	{
+		tmp = ft_deque_get_front(b);
+		ft_deque_pop_front(b);
+		ft_deque_push_back(b, tmp);
+	}
+	ft_display_stack(a, b);
+}
+
+void	ft_run_ope_rr(t_deque *a, t_deque *b, char *op)
+{
 	if (ft_strncmp("ra", op, ft_strlen("ra") + 1) == 0)
-	{
-		if (ft_deque_is_empty(a) == 0)
-		{
-			tmp = ft_deque_get_front(a);
-			ft_deque_pop_front(a);
-			ft_deque_push_back(a, tmp);
-		}
-		ft_display_stack(a, b);
-	}
+		_run_ra(a, b);
 	if (ft_strncmp("rb", op, ft_strlen("rb") + 1) == 0)
-	{
-		if (ft_deque_is_empty(b) == 0)
-		{
-			tmp = ft_deque_get_front(b);
-			ft_deque_pop_front(b);
-			ft_deque_push_back(b, tmp);
-		}
-		ft_display_stack(a, b);
-	}
+		_run_rb(a, b);
 	if (ft_strncmp("rr", op, ft_strlen("rr") + 1) == 0)
-	{
-		if (ft_deque_is_empty(a) == 0)
-		{
-			tmp = ft_deque_get_front(a);
-			ft_deque_pop_front(a);
-			ft_deque_push_back(a, tmp);
-		}
-		if (ft_deque_is_empty(b) == 0)
-		{
-			tmp = ft_deque_get_front(b);
-			ft_deque_pop_front(b);
-			ft_deque_push_back(b, tmp);
-		}
-		ft_display_stack(a, b);
-	}
+		_run_rr(a, b);
 }
--- src_checker/ft_run_ope_rrr.c
+++ src_checker/ft_run_ope_rrr.c
@@ -6,50 +6,63 @@
 /*   By: tayamamo <tayamamo@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/28 05:04:52 by tayamamo          #+#    #+#             */
-/*   Updated: 2021/04/28 05:05:19 by tayamamo         ###   ########.fr       */
+/*   Updated: 2021/04/28 05:14:49 by tayamamo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "checker.h"
 
-void	ft_run_ope_rrr(t_deque *a, t_deque *b, char *op)
+static void	_run_rra(t_deque *a, t_deque *b)
 {
 	int	tmp;
 
+	if (ft_deque_is_empty(a) == 0)
+	{
+		tmp = ft_deque_get_back(a);
+		ft_deque_pop_back(a);
+		ft_deque_push_front(a, tmp);
+	}
+	ft_display_stack(a, b);
+}
+
+static void	_run_rrb(t_deque *a, t_deque *b)
+{
+	int	tmp;
+
+	if (ft_deque_is_empty(b) == 0)
+	{
+		tmp = ft_deque_get_back(b);
+		ft_deque_pop_back(b);
+		ft_deque_push_front(b, tmp);
+	}
+	ft_display_stack(a, b);
+}
+
+static void	_run_rrr(t_deque *a, t_deque *b)
+{
+	int	tmp;
+
+	if (ft_deque_is_empty(a) == 0)
+	{
+		tmp = ft_deque_get_back(a);
+		ft_deque_pop_back(a);
+		ft_deque_push_front(a, tmp);
+	}
+	if (ft_deque_is_empty(b) == 0)
+	{
+		tmp = ft_deque_get_back(b);
+		ft_deque_pop_back(b);
+		ft_deque_push_front(b, tmp);
+	}
+	ft_display_stack(a, b);
+}
+
+void	ft_run_ope_rrr(t_deque *a, t_deque *b, char *op)
+{
 	if (ft_strncmp("rra", op, ft_strlen("rra") + 1) == 0)
-	{
-		if (ft_deque_is_empty(a) == 0)
-		{
-			tmp = ft_deque_get_back(a);
-			ft_deque_pop_back(a);
-			ft_deque_push_front(a, tmp);
-		}
-		ft_display_stack(a, b);
-	}
+		_run_rra(a, b);
 	if (ft_strncmp("rrb", op, ft_strlen("rrb") + 1) == 0)
-	{
-		if (ft_deque_is_empty(b) == 0)
-		{
-			tmp = ft_deque_get_back(b);
-			ft_deque_pop_back(b);
-			ft_deque_push_front(b, tmp);
-		}
-		ft_display_stack(a, b);
-	}
+		_run_rrb(a, b);
 	if (ft_strncmp("rrr", op, ft_strlen("rrr") + 1) == 0)
-	{
-		if (ft_deque_is_empty(a) == 0)
-		{
-			tmp = ft_deque_get_back(a);
-			ft_deque_pop_back(a);
-			ft_deque_push_front(a, tmp);
-		}
-		if (ft_deque_is_empty(b) == 0)
-		{
-			tmp = ft_deque_get_back(b);
-			ft_deque_pop_back(b);
-			ft_deque_push_front(b, tmp);
-		}
-		ft_display_stack(a, b);
-	}
+		_run_rrr(a, b);
 }
